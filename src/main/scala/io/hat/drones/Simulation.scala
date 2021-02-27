22ef6255ef4ab1913b09e903e731d19907a74b45
martin.hatas <martin.hatas@pricefx.eu>
2021-02-27 17:17:56 +0100

    Tube Map actor returns nearbby stations

--- src/main/scala/io/hat/drones/Simulation.scala
+++ src/main/scala/io/hat/drones/Simulation.scala
@@ -12,14 +12,20 @@ import io.hat.drones.TubeMap.{Station, TubeMapProtocol}
 import java.nio.file.Paths
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
+import scala.util.{Success, Try}
 
 object Dispatcher {
 
   trait DispatcherProtocol
 
-  def apply(drones: Map[String, ActorRef[DroneProtocol]]): Behavior[DispatcherProtocol] = Behaviors.receive { (context, message) =>
-    Behaviors.same
+  def apply(drones: Map[String, ActorRef[DroneProtocol]]): Behavior[DispatcherProtocol] = Behaviors.setup { context =>
+
+    Behaviors.receive { (context, message) =>
+
+      Behaviors.same
+    }
   }
+
 }
 
 object TrafficDrone {
@@ -36,18 +42,44 @@ object TrafficDrone {
 
 object TubeMap {
 
-  trait TubeMapProtocol
-  case class ScanStationsRequest(lat: Double, lon: Double, radius: Int, replyTo: ActorRef[_]) extends TubeMapProtocol
-  case class Station(name: String, lat: Double, lon: Double)
-  case class StationsAroundResponse(stations: Set[Station])
+  val EarthRadiusMeters = 6378137d
 
-  def apply(stations: Seq[Station]): Behavior[TubeMapProtocol] = {
+  trait TubeMapProtocol
+  case class ScanStationsRequest(lat: Double, lon: Double, radiusMeters: Int, replyTo: ActorRef[StationsInRadiusResponse]) extends TubeMapProtocol
+  case class Station(name: String, lat: Double, lon: Double)
+  case class StationsInRadiusResponse(stations: Set[Station])
+
+  def apply(stations: Set[Station]): Behavior[TubeMapProtocol] = {
     Behaviors.receive[TubeMapProtocol] { (context, msg) =>
       msg match {
-        case ScanStationsRequest(lat, lon, radius, replyTo) =>
+        case ScanStationsRequest(lat, lon, radius, replyTo) if positive(radius) && validLat(lat) && validLon(lon) =>
+          val stationsInRadius = stations
+            .filterNot(station => radius <= haversineDistance(lat, lon, station) )
+          replyTo ! StationsInRadiusResponse(stationsInRadius)
+        case ScanStationsRequest(_, _, _, replyTo) => replyTo ! StationsInRadiusResponse(Set())
       }
       Behaviors.same
     }
+  }
+
+  def validLat(lat: Double) = -90 <= lat && lat <= 90
+
+  def validLon(lat: Double) = -180 <= lat && lat <= 120
+
+  def positive(radius: Int) = radius >= 0
+
+  def haversineDistance(droneLat: Double, droneLon: Double, station: Station): Double = {
+    val deltaLat = math.toRadians(station.lat - droneLat)
+    val deltaLon = math.toRadians(station.lon - droneLon)
+    val droneLatRad = math.toRadians(droneLat)
+    val stationLatRad = math.toRadians(station.lat)
+
+    val a = math.pow(math.sin(deltaLat / 2), 2) +
+        math.pow(math.sin(deltaLon / 2), 2) * math.cos(droneLatRad) * math.cos(stationLatRad)
+    val c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    val distanceMeters = EarthRadiusMeters * c
+    distanceMeters
   }
 
 }
@@ -67,16 +99,17 @@ object Simulation {
         val stationsLoading = FileIO.fromPath(Paths.get(startMessage.mapFileName))
           .via(CsvParsing.lineScanner())
           .map(csvRecord =>
-            Station(
+            Try(Station(
               name = csvRecord(0).utf8String,
               lat = csvRecord(1).utf8String.toDouble,
               lon = csvRecord(2).utf8String.toDouble)
-            )
+            ))
+          .collect{ case Success(station) => station }
           .log("Tube station registered").addAttributes(Attributes.logLevels(onElement = Attributes.LogLevels.Info))
           .runWith(Sink.seq)
 
         val stations = Await.result(stationsLoading, 5 seconds)
-        val tubeMap = context.spawn(TubeMap(stations), "tube-map")
+        val tubeMap = context.spawn(TubeMap(stations.toSet), "tube-map")
 
         val drones = startMessage.dronesIds
           .map(droneId => (droneId -> context.spawn(TrafficDrone(droneId, tubeMap), s"drone-$droneId")))
--- src/test/scala/io/hat/drones/TubeMapSpec.scala
+++ src/test/scala/io/hat/drones/TubeMapSpec.scala
@@ -1,23 +1,85 @@
 package io.hat.drones
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import io.hat.drones.TubeMap.{ScanStationsRequest, Station, StationsInRadiusResponse}
 import org.scalatest.wordspec.AnyWordSpecLike
 
 class TubeMapSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
 
   "TubeMap" must {
 
-    "responds with none tube stations in radius" in {}
+    val stations = Set(
+      Station("Acton Town", 51.503071, -0.280303),
+      Station("Aldgate", 51.514342, -0.075627),
+      Station("Aldgate East", 51.51503, -0.073162),
+      Station("All Saints (DLR)", 51.510477, -0.012625),
+      Station("Alperton", 51.541209, -0.299516)
+    )
 
-    "responds with single tube stations in radius" in {}
+    val tubeMap = testKit.spawn(TubeMap(stations), "tube-map-under-test")
 
-    "responds with multiple tube stations in radius" in {}
+    "responds with none tube stations in radius" in {
+      val (lat, lon) = (52.503071, -0.280303)
+      val probe = testKit.createTestProbe[StationsInRadiusResponse]()
 
-    "respond with empty stations list if on requests with invalid latitude" in {}
+      tubeMap ! ScanStationsRequest(lat, lon, radiusMeters = 300, replyTo = probe.ref)
 
-    "respond with empty stations list if on requests with invalid longtitude" in {}
+      probe.expectMessage(StationsInRadiusResponse( Set() ))
+    }
 
-    "respond with empty stations list if on requests with invalid radius" in {}
+    "responds with single tube stations in radius" in {
+      val (lat, lon) = (51.503071, -0.280303)
+      val probe = testKit.createTestProbe[StationsInRadiusResponse]()
+
+      tubeMap ! ScanStationsRequest(lat, lon, radiusMeters = 300, replyTo = probe.ref)
+
+      probe.expectMessage(
+        StationsInRadiusResponse( Set(Station("Acton Town", 51.503071, -0.280303)))
+      )
+    }
+
+    "responds with multiple tube stations in radius" in {
+      val (lat, lon) = (51.503071, -0.280303)
+      val probe = testKit.createTestProbe[StationsInRadiusResponse]()
+
+      tubeMap ! ScanStationsRequest(lat, lon, radiusMeters = 15_000, replyTo = probe.ref)
+
+      probe.expectMessage(
+        StationsInRadiusResponse( Set(
+          Station("Acton Town", 51.503071,-0.280303),
+          Station("Aldgate East", 51.51503,-0.073162),
+          Station("Alperton", 51.541209,-0.299516),
+          Station("Aldgate", 51.514342,-0.075627)
+        ))
+      )
+    }
+
+    "respond with empty stations list if on requests with invalid latitude" in {
+      val (invalidLat, lon) = (100, -0.280303)
+      val probe = testKit.createTestProbe[StationsInRadiusResponse]()
+
+      tubeMap ! ScanStationsRequest(invalidLat, lon, radiusMeters = 300, replyTo = probe.ref)
+
+      probe.expectMessage(StationsInRadiusResponse( Set() ))
+    }
+
+    "respond with empty stations list if on requests with invalid longtitude" in {
+      val (lat, invalidLon) = (1.503071, -200)
+      val probe = testKit.createTestProbe[StationsInRadiusResponse]()
+
+      tubeMap ! ScanStationsRequest(lat, invalidLon, radiusMeters = 300, replyTo = probe.ref)
+
+      probe.expectMessage(StationsInRadiusResponse( Set() ))
+    }
+
+    "respond with empty stations list if on requests with negative radius" in {
+      val (lat, lon) = (52.503071, -0.280303)
+      val probe = testKit.createTestProbe[StationsInRadiusResponse]()
+
+      tubeMap ! ScanStationsRequest(lat, lon, radiusMeters = -300, replyTo = probe.ref)
+
+      probe.expectMessage(StationsInRadiusResponse( Set() ))
+    }
 
   }
 
